1dae905c21c9fca517920c490f4f97543180cef0
Hossein Amer <hossein.esmat@gmail.com>
2025-06-28 15:18:36 +0200

    Done with it for real?

--- src/data/games.ts
+++ src/data/games.ts
@@ -1,5 +1,48 @@
 import { Game } from '../types/Game';
 
+// Import all images
+// Import all images
+import catanImage from '../pictures/catan.avif';
+import cardsAgainstHumanityImage from '../pictures/cardsagainsthumanity.webp';
+import wingspanImage from '../pictures/wingspan.jpeg';
+import ticketToRideImage from '../pictures/TicketToRide.webp';
+import azulImage from '../pictures/azul.jpg';
+import pandemicImage from '../pictures/pandemic.webp';
+import disturbedFriendsImage from '../pictures/distuberedFriends.webp';
+import jokingHazardImage from '../pictures/JokingHazard.webp';
+import incoherentImage from '../pictures/incoherent.jpg';
+import camelUpImage from '../pictures/camelup.jpg';
+import tokyoHighwayImage from '../pictures/tokyohighway.webp';
+import terraformingMarsImage from '../pictures/terraformingMars.avif';
+import splendorImage from '../pictures/splendor.webp';
+import whatDoYouMemeImage from '../pictures/whatdoyoumeme.webp';
+import explodingKittensImage from '../pictures/explodingkittens.webp';
+import redFlagsImage from '../pictures/redflags.jpg';
+import throwThrowBurritoImage from '../pictures/throwthrowburrito.jpg';
+import nightOfTheNinjaImage from '../pictures/nightoftheninja.webp';
+import justOneImage from '../pictures/justone.webp';
+import unoImage from '../pictures/uno.webp';
+import fluxxImage from '../pictures/fluxx.jpeg';
+import theMindImage from '../pictures/themind.webp';
+import brassBirminghamImage from '../pictures/brassbirmingham.webp';
+import codenamesImage from '../pictures/codesnames.jpg';
+import monopolyImage from '../pictures/monopoly.jpg';
+import scrabbleImage from '../pictures/scrabble.webp';
+import kingOfTokyoImage from '../pictures/kingoftoyko.webp';
+import loveLetterImage from '../pictures/loveletter.webp';
+import dixitImage from '../pictures/dixit.webp';
+import yahtzeeImage from '../pictures/yatzy.jpg';
+import sushiGoImage from '../pictures/sushigo.webp';
+import machiKoroImage from '../pictures/IDW6650_1.webp';
+import clueImage from '../pictures/clue.jpg';
+import betrayalImage from '../pictures/betrayal.jpg';
+import gloomhavenImage from '../pictures/gloomhaven.jpg';
+import riskImage from '../pictures/risk.avif';
+import pictionaryImage from '../pictures/pictionary.jpg';
+import munchkinImage from '../pictures/munchkin.webp';
+import spotItImage from '../pictures/spotit.png';
+import hitsterImage from '../pictures/hitster.webp';
+
 export const boardGames: Game[] = [
   {
     id: '1',
@@ -10,7 +53,7 @@ export const boardGames: Game[] = [
     difficulty: 3,
     learningCurve: 2,
     category: ['Strategy', 'Trading', 'Family'],
-    image: 'src/data/pictures/catan.avif',
+    image: catanImage, // Using imported image
     youtubeUrl: 'https://www.youtube.com/watch?v=oiQ6SgBzfqY&pp=ygURY2F0YW4gaG93IHRvIHBsYXk%3D',
     manualUrl: 'https://www.catan.com/sites/default/files/2021-06/catan_base_rules_2020_200707.pdf',
     rating: 4.5
@@ -24,7 +67,7 @@ export const boardGames: Game[] = [
     difficulty: 1,
     learningCurve: 1,
     category: ['Party', 'Humor', 'Adult'],
-    image: 'src/data/pictures/cardsagainsthumanity.webp',
+    image: cardsAgainstHumanityImage, // Using imported image
     youtubeUrl: 'https://www.youtube.com/watch?v=QCEqUn7If44',
     manualUrl: 'https://s3.amazonaws.com/cah/CAH_Rules.pdf',
     rating: 4.2
@@ -38,11 +81,12 @@ export const boardGames: Game[] = [
     difficulty: 3,
     learningCurve: 3,
     category: ['Strategy', 'Engine Building', 'Nature'],
-    image: 'src/data/pictures/wingspan.jpeg',
+    image: wingspanImage, // Using imported image
     youtubeUrl: 'https://www.youtube.com/watch?v=lgDgcLI2B0U',
     manualUrl: 'https://stonemaiergames.com/games/wingspan/rules/',
     rating: 4.7
   },
+  // Continue with all other games using imported images...
   {
     id: '4',
     name: 'Ticket to Ride',
@@ -52,7 +96,7 @@ export const boardGames: Game[] = [
     difficulty: 2,
     learningCurve: 2,
     category: ['Strategy', 'Route Building', 'Family'],
-    image: 'src/data/pictures/TicketToRide.webp',
+    image: ticketToRideImage,
     youtubeUrl: 'https://www.youtube.com/watch?v=qHmf1bau9xQ',
     manualUrl: 'https://ncdn0.daysofwonder.com/tickettoride/en/img/tt_rules_2015_en.pdf',
     rating: 4.4
@@ -66,7 +110,7 @@ export const boardGames: Game[] = [
     difficulty: 2,
     learningCurve: 2,
     category: ['Strategy', 'Tile Placement', 'Abstract'],
-    image: 'src/data/pictures/azul.jpg',
+    image: azulImage,
     youtubeUrl: 'https://www.youtube.com/watch?v=193R2h2M3Yk&ab_channel=TripleSGames',
     manualUrl: 'https://tesera.ru/images/items/1108676/EN-Azul-Rules.pdf',
     rating: 4.6
@@ -80,7 +124,7 @@ export const boardGames: Game[] = [
     difficulty: 3,
     learningCurve: 3,
     category: ['Cooperative', 'Strategy', 'Medical'],
-    image: 'src/data/pictures/pandemic.webp',
+    image: pandemicImage,
     youtubeUrl: 'https://www.youtube.com/watch?v=63Ha1ktxvoY&ab_channel=Geek%26Sundry',
     manualUrl: 'https://cdn.svc.asmodee.net/production-zman/uploads/2024/09/Pandemic_Rulebook.pdf',
     rating: 4.3
@@ -94,7 +138,7 @@ export const boardGames: Game[] = [
     difficulty: 1,
     learningCurve: 1,
     category: ['Party', 'Adult Humor', 'Social'],
-    image: 'src/data/pictures/distuberedFriends.webp',
+    image: disturbedFriendsImage,
     youtubeUrl: 'https://www.youtube.com/watch?v=tMrZVG2hQz8&ab_channel=HighlyRecommendedProducts',
     manualUrl: 'https://www.disturbedfriends.com/',
     rating: 4.1
@@ -108,7 +152,7 @@ export const boardGames: Game[] = [
     difficulty: 1,
     learningCurve: 1,
     category: ['Party', 'Adult Humor', 'Creative'],
-    image: 'src/data/pictures/JokingHazard.webp',
+    image: jokingHazardImage,
     youtubeUrl: 'https://www.youtube.com/watch?v=VHquyO11uSM&ab_channel=RundliiGaming',
     manualUrl: 'https://www.kickstarter.com/projects/explosm/joking-hazard',
     rating: 4.2
@@ -122,7 +166,7 @@ export const boardGames: Game[] = [
     difficulty: 2,
     learningCurve: 1,
     category: ['Party', 'Word Game', 'Social'],
-    image: 'src/data/pictures/incoherent.jpg',
+    image: incoherentImage,
     youtubeUrl: 'https://www.youtube.com/watch?v=psFtfrNSWvM&ab_channel=PsyCatGames',
     manualUrl: 'https://www.whatdoyoumeme.com/products/incohearent',
     rating: 3.9
@@ -136,7 +180,7 @@ export const boardGames: Game[] = [
     difficulty: 2,
     learningCurve: 2,
     category: ['Family', 'Racing', 'Betting'],
-    image: 'src/data/pictures/camelup.jpg',
+    image: camelUpImage,
     youtubeUrl: 'https://www.youtube.com/watch?v=ylpy5i7_w5s&t=78s&ab_channel=TheCasualGamers',
     manualUrl: 'https://cdn.1j1ju.com/medias/8b/c6/82-camel-up-rulebook.pdf',
     rating: 4.0
@@ -150,7 +194,7 @@ export const boardGames: Game[] = [
     difficulty: 2,
     learningCurve: 2,
     category: ['Dexterity', 'Construction', 'Abstract'],
-    image: 'src/data/pictures/tokyohighway.webp',
+    image: tokyoHighwayImage,
     youtubeUrl: 'https://www.youtube.com/watch?v=lBokX76WH_Q',
     manualUrl: 'https://www.itten-games.com/wp-content/uploads/2019/02/TOKYO_HIGHWAY_rule_en.pdf',
     rating: 4.2
@@ -164,7 +208,7 @@ export const boardGames: Game[] = [
     difficulty: 4,
     learningCurve: 4,
     category: ['Strategy', 'Engine Building', 'Science Fiction'],
-    image: 'src/data/pictures/terraformingMars.avif',
+    image: terraformingMarsImage,
     youtubeUrl: 'https://www.youtube.com/watch?v=n3yVpsiVwL8',
     manualUrl: 'https://cdn.1j1ju.com/medias/13/3f/fb-terraforming-mars-rule.pdf',
     rating: 4.5
@@ -178,7 +222,7 @@ export const boardGames: Game[] = [
     difficulty: 2,
     learningCurve: 2,
     category: ['Strategy', 'Engine Building', 'Economic'],
-    image: 'src/data/pictures/splendor.webp',
+    image: splendorImage,
     youtubeUrl: 'https://www.youtube.com/watch?v=6Rk4nKEYrhY&ab_channel=GamePoint',
     manualUrl: 'https://cdn.1j1ju.com/medias/7f/91/ba-splendor-rulebook.pdf',
     rating: 4.1
@@ -192,7 +236,7 @@ export const boardGames: Game[] = [
     difficulty: 1,
     learningCurve: 1,
     category: ['Party', 'Social Media', 'Humor'],
-    image: 'src/data/pictures/whatdoyoumeme.webp',
+    image: whatDoYouMemeImage,
     youtubeUrl: 'https://www.youtube.com/watch?v=UaWOmfuzcTY&ab_channel=TripleSGames',
     manualUrl: 'https://cdn.1j1ju.com/medias/e9/94/bd-what-do-you-meme-rulebook.pdf',
     rating: 3.8
@@ -206,7 +250,7 @@ export const boardGames: Game[] = [
     difficulty: 1,
     learningCurve: 1,
     category: ['Card Game', 'Party'],
-    image: 'src/data/pictures/explodingkittens.webp',
+    image: explodingKittensImage,
     youtubeUrl: 'https://www.youtube.com/watch?v=O3BDwMx3FpE&ab_channel=TripleSGames',
     manualUrl: 'https://ek.explodingkittens.com/downloads/rules/EK_Party_Pack-Rules_wAttack.pdf',
     rating: 3.7
@@ -220,7 +264,7 @@ export const boardGames: Game[] = [
     difficulty: 1,
     learningCurve: 1,
     category: ['Party', 'Adult Humor', 'Social'],
-    image: 'src/data/pictures/redflags.jpg',
+    image: redFlagsImage,
     youtubeUrl: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
     manualUrl: 'https://www.youtube.com/watch?v=ssj3rGZpvfg&ab_channel=MatMousallam',
     rating: 4.0
@@ -234,7 +278,7 @@ export const boardGames: Game[] = [
     difficulty: 1,
     learningCurve: 1,
     category: ['Party', 'Action'],
-    image: 'src/data/pictures/throwthrowburrito.jpg',
+    image: throwThrowBurritoImage,
     youtubeUrl: 'https://www.youtube.com/watch?v=I4Kq-H902uo&ab_channel=ExplodingKittens',
     manualUrl: 'https://dumekj556jp75.cloudfront.net/ttb/burrito-english.pdf',
     rating: 4.1
@@ -248,7 +292,7 @@ export const boardGames: Game[] = [
     difficulty: 2,
     learningCurve: 2,
     category: ['Strategy', 'Family'],
-    image: 'src/data/pictures/nightoftheninja.webp',
+    image: nightOfTheNinjaImage,
     youtubeUrl: 'https://www.youtube.com/watch?v=QHRunh6PyjQ&ab_channel=BrotherwiseGames',
     manualUrl: 'https://fgbradleys.com/wp-content/uploads/Night-of-the-Ninja-Rulebook.pdf?srsltid=AfmBOooZx2g0JM9cCf89sNS1xgwADHp8zP_tVip9G1E3RcqmUROCSkEB',
     rating: 3.9
@@ -262,7 +306,7 @@ export const boardGames: Game[] = [
     difficulty: 1,
     learningCurve: 1,
     category: ['Party', 'Cooperative', 'Word Game', 'Drawing'],
-    image: 'src/data/pictures/justone.webp',
+    image: justOneImage,
     youtubeUrl: 'https://www.youtube.com/watch?v=4ulLmUQ7omU&ab_channel=AsmodeeNorthAmerica',
     manualUrl: 'https://cdn.1j1ju.com/medias/1f/0e/8f-just-one-rulebook.pdf',
     rating: 4.2
@@ -276,7 +320,7 @@ export const boardGames: Game[] = [
     difficulty: 1,
     learningCurve: 1,
     category: ['Card Game', 'Family'],
-    image: 'src/data/pictures/uno.webp',
+    image: unoImage,
     youtubeUrl: 'https://www.youtube.com/watch?v=FkuqYtE1rw0&ab_channel=TripleSGames',
     manualUrl: 'https://www.mattelgames.com/en-us/cards/uno',
     rating: 3.5
@@ -290,7 +334,7 @@ export const boardGames: Game[] = [
     difficulty: 2,
     learningCurve: 2,
     category: ['Card Game', 'Family'],
-    image: 'src/data/pictures/fluxx.jpeg',
+    image: fluxxImage,
     youtubeUrl: 'https://www.youtube.com/watch?v=VtNtUzx7dnY&ab_channel=Boardgaimz',
     manualUrl: 'https://www.looneylabs.com/games/fluxx',
     rating: 3.6
@@ -304,7 +348,7 @@ export const boardGames: Game[] = [
     difficulty: 2,
     learningCurve: 2,
     category: ['Cooperative', 'Card Game'],
-    image: 'src/data/pictures/themind.webp',
+    image: theMindImage,
     youtubeUrl: 'https://www.youtube.com/watch?v=uXl8MC0GMYE&t=448s&ab_channel=WatchItPlayed',
     manualUrl: 'https://thesolomeeple.com/2019/11/12/the-mind/',
     rating: 4.1
@@ -318,7 +362,7 @@ export const boardGames: Game[] = [
     difficulty: 5,
     learningCurve: 5,
     category: ['Strategy', 'Economic','Tile Placement'],
-    image: 'src/data/pictures/brassbirmingham.webp',
+    image: brassBirminghamImage,
     youtubeUrl: 'https://www.youtube.com/watch?v=Lzxf4tfq9as&t=275s&ab_channel=WatchItPlayed',
     manualUrl: 'https://cdn.1j1ju.com/medias/60/39/64-brass-birmingham-rulebook.pdf',
     rating: 4.7
@@ -332,7 +376,7 @@ export const boardGames: Game[] = [
     difficulty: 2,
     learningCurve: 2,
     category: ['Party', 'Word Game', 'Deduction'],
-    image: 'src/data/pictures/codesnames.jpg',
+    image: codenamesImage,
     youtubeUrl: 'https://www.youtube.com/watch?v=cueZnzkSL20&ab_channel=CardboardCrashCourse',
     manualUrl: 'https://www.czechgames.com/games/codenames#downloads',
     rating: 4.3
@@ -346,7 +390,7 @@ export const boardGames: Game[] = [
     difficulty: 2,
     learningCurve: 2,
     category: ['Family', 'Economic', 'Classic'],
-    image: 'src/data/pictures/monopoly.jpg',
+    image: monopolyImage,
     youtubeUrl: 'https://www.youtube.com/watch?v=AuWvMgYv03g&ab_channel=GatherTogetherGames',
     manualUrl: 'https://www.hasbro.com/common/instruct/00009.pdf',
     rating: 3.2
@@ -360,7 +404,7 @@ export const boardGames: Game[] = [
     difficulty: 3,
     learningCurve: 2,
     category: ['Word Game', 'Family', 'Classic'],
-    image: 'src/data/pictures/scrabble.webp',
+    image: scrabbleImage,
     youtubeUrl: 'https://www.youtube.com/watch?v=A9fJT13NT6g&ab_channel=TripleSGames',
     manualUrl: 'https://www.hasbro.com/common/instruct/Scrabble_(2003).pdf',
     rating: 3.8
@@ -374,7 +418,7 @@ export const boardGames: Game[] = [
     difficulty: 2,
     learningCurve: 2,
     category: ['Dice Rolling', 'Family'],
-    image: 'src/data/pictures/kingoftoyko.webp',
+    image: kingOfTokyoImage,
     youtubeUrl: 'https://www.youtube.com/watch?v=_cwIdBgA_MQ&ab_channel=GamesMadeEasy',
     manualUrl: 'https://cdn.1j1ju.com/medias/f9/2f/9b-king-of-tokyo-rulebook.pdf',
     rating: 4.1
@@ -388,7 +432,7 @@ export const boardGames: Game[] = [
     difficulty: 1,
     learningCurve: 1,
     category: ['Card Game', 'Deduction', 'Family'],
-    image: 'src/data/pictures/loveletter.webp',
+    image: loveLetterImage,
     youtubeUrl: 'https://www.youtube.com/watch?v=RiQ2To5KWJM&ab_channel=WatchItPlayed',
     manualUrl: 'https://cdn.svc.asmodee.net/production-zman/uploads/2024/09/LL_Rulebook_with_Bag-1.pdf',
     rating: 3.9
@@ -402,7 +446,7 @@ export const boardGames: Game[] = [
     difficulty: 2,
     learningCurve: 2,
     category: ['Party','Mystery'],
-    image: 'src/data/pictures/dixit.webp',
+    image: dixitImage,
     youtubeUrl: 'https://www.youtube.com/watch?v=Qi4MoW6NuaQ&ab_channel=TripleSGames',
     manualUrl: 'https://cdn.1j1ju.com/medias/8f/03/3d-dixit-rulebook.pdf',
     rating: 4.2
@@ -416,7 +460,7 @@ export const boardGames: Game[] = [
     difficulty: 1,
     learningCurve: 1,
     category: ['Dice Rolling', 'Family', 'Classic'],
-    image: 'src/data/pictures/yatzy.jpg',
+    image: yahtzeeImage,
     youtubeUrl: 'https://www.youtube.com/watch?v=AHDgpuEzopc',
     manualUrl: 'https://info.lite.games/en/support/solutions/articles/60000688821-yatzy-rules',
     rating: 3.4
@@ -430,7 +474,7 @@ export const boardGames: Game[] = [
     difficulty: 1,
     learningCurve: 1,
     category: ['Card Game', 'Family'],
-    image: 'src/data/pictures/sushigo.webp',
+    image: sushiGoImage,
     youtubeUrl: 'https://www.youtube.com/watch?v=-WO1cP9wzrw&ab_channel=GamesExplained',
     manualUrl: 'https://cdn.1j1ju.com/medias/e5/92/74-sushi-go-rulebook.pdf',
     rating: 4.0
@@ -444,7 +488,7 @@ export const boardGames: Game[] = [
     difficulty: 2,
     learningCurve: 2,
     category: ['Dice Rolling', 'Strategy', 'Family'],
-    image: 'src/data/pictures/IDW6650_1.webp',
+    image: machiKoroImage,
     youtubeUrl: 'https://www.youtube.com/watch?v=X6y02IGRE-U&ab_channel=WatchItPlayed',
     manualUrl: 'https://cdn.1j1ju.com/medias/89/cb/e8-machi-koro-rulebook.pdf',
     rating: 3.8
@@ -458,7 +502,7 @@ export const boardGames: Game[] = [
     difficulty: 2,
     learningCurve: 2,
     category: ['Deduction', 'Mystery', 'Classic','Dice Rolling'],
-    image: 'src/data/pictures/clue.jpg',
+    image: clueImage,
     youtubeUrl: 'https://www.youtube.com/watch?v=LTUFY0URGQo&ab_channel=TabletopDuo',
     manualUrl: 'https://www.hasbro.com/common/documents/dad2885d1c4311ddbd0b0800200c9a66/2BFAEC9E5056900B102C3859E9AC6332.pdf',
     rating: 3.6
@@ -472,7 +516,7 @@ export const boardGames: Game[] = [
     difficulty: 3,
     learningCurve: 3,
     category: ['Horror', 'Exploration', 'Betrayal'],
-    image: 'src/data/pictures/betrayal.jpg',
+    image: betrayalImage,
     youtubeUrl: 'https://www.youtube.com/watch?v=hG9XL4zQ2Ck',
     manualUrl: 'https://boardgame.bg/betrayal%20at%20house%20on%20the%20hill%20rules.pdf',
     rating: 4.0
@@ -486,7 +530,7 @@ export const boardGames: Game[] = [
     difficulty: 5,
     learningCurve: 5,
     category: ['Dungeon Crawler','Strategy','Cooperative'],
-    image: 'src/data/pictures/gloomhaven.jpg',
+    image: gloomhavenImage,
     youtubeUrl: 'https://www.youtube.com/watch?v=8jIwaVb4oXY&ab_channel=ModernCardboard',
     manualUrl: 'https://cdn.1j1ju.com/medias/8d/c5/21-gloomhaven-rulebook.pdf',
     rating: 4.8
@@ -500,11 +544,10 @@ export const boardGames: Game[] = [
     difficulty: 3,
     learningCurve: 3,
     category: ['Strategy','Classic'],
-    image: 'src/data/pictures/risk.avif',
+    image: riskImage,
     youtubeUrl: 'https://www.youtube.com/watch?v=wr5qMMwfUMk&ab_channel=TripleSGames',
     manualUrl: 'https://www.hasbro.com/common/instruct/risk.pdf',
-    rating: 3.7,
-    
+    rating: 3.7
   },
   {
     id: '37',
@@ -515,11 +558,10 @@ export const boardGames: Game[] = [
     difficulty: 1,
     learningCurve: 1,
     category: ['Party', 'Drawing', 'Cooperative'],
-    image: 'src/data/pictures/pictionary.jpg',
+    image: pictionaryImage,
     youtubeUrl: 'https://www.youtube.com/watch?v=CXUWdmZv-cg',
     manualUrl: 'https://www.hasbro.com/common/instruct/pictionary.pdf',
-    rating: 3.9,
-    
+    rating: 3.9
   },
   {
     id: '38',
@@ -530,11 +572,10 @@ export const boardGames: Game[] = [
     difficulty: 2,
     learningCurve: 2,
     category: ['Card Game', 'Humor','Betrayal'],
-    image: 'src/data/pictures/munchkin.webp',
+    image: munchkinImage,
     youtubeUrl: 'https://www.youtube.com/watch?v=WTFCWILpEo8&ab_channel=RollForCrit',
     manualUrl: 'https://munchkin.game/site-munchkin/assets/files/1138/munchkin_rules-1.pdf',
-    rating: 3.8,
-
+    rating: 3.8
   },
   {
     id: '39',
@@ -545,12 +586,12 @@ export const boardGames: Game[] = [
     difficulty: 1,
     learningCurve: 1,
     category: ['Party', 'Family'],
-    image: 'src/data/pictures/spotit.png',
+    image: spotItImage,
     youtubeUrl: 'https://www.youtube.com/watch?v=Y6N6UmsNGsk&ab_channel=BrimleyGames',
     manualUrl: 'https://www.ipswichlibrary.org/wp-content/uploads/2021/04/IPL-LOT-Manual-Spot-It-Game-Instructions.pdf',
-    rating: 4.0,
-   
-  }, {
+    rating: 4.0
+  },
+  {
     id: '40',
     name: 'Hitster',
     description: 'A fast-paced game which you have to guess the song between time lines',
@@ -559,13 +600,11 @@ export const boardGames: Game[] = [
     difficulty: 1,
     learningCurve: 1,
     category: ['Party', 'Family','Social Media'],
-    image: 'src/data/pictures/hitster.webp',
+    image: hitsterImage,
     youtubeUrl: 'https://www.youtube.com/watch?v=Y_NW6iZh1O0&ab_channel=SPIELREGELNTV',
     manualUrl: 'https://nordics.hitstergame.com/sv-en/how-to-play/',
-    rating: 4.0,
-   
+    rating: 4.0
   }
-
 ];
 
 export interface ChatMessage {
@@ -573,8 +612,8 @@ export interface ChatMessage {
   text: string;
   isUser: boolean;
   timestamp: Date;
-  gameLink?: string; // Keep this for backward compatibility
-  recommendedGames?: Game[]; // New field for multiple game recommendations
+  gameLink?: string;
+  recommendedGames?: Game[];
 }
 
 export const gameCategories = [
